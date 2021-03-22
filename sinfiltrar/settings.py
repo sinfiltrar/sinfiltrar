802be6e988543d2e237a68419630647549c8ce76
Mauro Spivak <mauros@gmail.com>
2021-03-21 21:36:58 -0300

    another dummy change to trigger build

--- sinfiltrar/settings.py
+++ sinfiltrar/settings.py
@@ -74,10 +74,10 @@ WSGI_APPLICATION = 'sinfiltrar.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
 }
 
 
