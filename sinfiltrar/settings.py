e39131ff84d863ffd3587d4432aed52a5b1e264d
Mauro Spivak <mauros@gmail.com>
2021-03-22 19:10:12 -0300

    Adding CORS!

--- sinfiltrar/settings.py
+++ sinfiltrar/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'issuers.apps.IssuersConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://sinfiltr.ar",
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'sinfiltrar.urls'
