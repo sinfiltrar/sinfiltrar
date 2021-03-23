515878ad2860b986edfd5401141eff723626314c
Mauro Spivak <mauros@gmail.com>
2021-03-23 16:27:34 -0300

    IssuerEmail model

--- issuers/models.py
+++ issuers/models.py
@@ -1,8 +1,11 @@
-from django.db import models;
+from django.db import models
 
 class Issuer(models.Model):
     slug = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
-    created_at = models.DateTimeField();
+    created_at = models.DateTimeField(auto_now_add=True)
 
-
+class IssuerEmail(models.Model):
+    email = models.CharField(max_length=254)
+    issuer = models.ForeignKey('Issuer', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
