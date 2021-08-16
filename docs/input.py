81d8d6786b40e7c430e931cd6e53b7e822383db3
Mauro Spivak <mauros@gmail.com>
2021-08-15 23:47:59 -0300

    Process all emails from s3

--- docs/input.py
+++ docs/input.py
@@ -1,4 +1,8 @@
 import json
+import boto3
+
+from django.conf import settings
+
 from docs.models import Doc
 
 
@@ -8,3 +12,16 @@ def process_s3_input(event):
 	doc.save()
 
 
+def process_existing_s3():
+	s3 = boto3.resource('s3', )
+	sns = boto3.client('sns')
+	bucket = s3.Bucket(settings.AWS_S3_BUCKET_NAME_INPUT)
+
+	print('Connected to bucket')
+
+	for object in bucket.objects.all():
+		print(f'Sending SNS message for email {object.key}',)
+		response = sns.publish(
+			TopicArn=f'arn:aws:sns:us-west-2:153920312805:{settings.AWS_S3_BUCKET_NAME_INPUT}',
+			Message=json.dumps({'receipt': {'action': {'bucketName': object.bucket_name, 'objectKey': object.key}}}),
+		)
