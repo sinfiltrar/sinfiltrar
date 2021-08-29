8f5441fc06eb5138592e14331c8b8d82f22b647f
Mauro Spivak <mauros@gmail.com>
2021-08-29 11:28:36 -0300

    AWS Creds not needed here

--- sinfiltrar/settings.py
+++ sinfiltrar/settings.py
@@ -152,9 +152,6 @@ USE_TZ = True
 USE_S3 = os.getenv('USE_S3') == 'TRUE'
 
 if USE_S3:
-    # aws settings
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_BUCKET_NAME_STATIC = "sinfiltrar-zappa-static"
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_S3_BUCKET_NAME_STATIC
