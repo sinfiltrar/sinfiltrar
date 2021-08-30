91f7ba5e6879a87b9d281bd119b2bfa49a9696c1
Mauro Spivak <mauros@gmail.com>
2021-08-30 18:18:31 -0300

    More love

--- docs/admin.py
+++ docs/admin.py
@@ -4,5 +4,6 @@ from docs.models import Doc
 
 @admin.register(Doc)
 class DocAdmin(admin.ModelAdmin):
-	date_hierarchy = 'issued_at'
-	list_display = ('title', 'from_email', 'issued_at', 'issuer')
+    date_hierarchy = 'issued_at'
+    list_display = ('title', 'from_email', 'issued_at', 'issuer')
+    list_filter = ('issuer', )
