42e73a0d975465ed2ae0807f5eb3eda24bf9501e
Mauro Spivak <mauros@gmail.com>
2021-08-30 17:02:24 -0300

    New About page, Us page

--- sinfiltrar/urls.py
+++ sinfiltrar/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('us/', TemplateView.as_view(template_name='us.html'), name='us'),
 ]
