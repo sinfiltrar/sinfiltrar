42c3a49705e8bf612590bbe540652d64cba47125
Iván Raskovsky <raskovsky+git@gmail.com>
2021-09-01 09:52:14 -0300

    add status and info for Issuer

--- issuers/admin.py
+++ issuers/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from issuers.models import Issuer, IssuerEmail
 
+
 @admin.register(Issuer)
 class IssuerAdmin(admin.ModelAdmin):
-	list_display = ('id', 'slug', 'name')
+    list_display = ('name', 'slug', 'status', 'created_at', )
+    list_filter = ('status', )
+    list_search = ('name', 'slug', 'info', )
+    date_hierarchy = 'created_at'
 
 
 @admin.register(IssuerEmail)
 class IssuerEmailAdmin(admin.ModelAdmin):
-	list_display = ('id', 'email')
+    list_display = ('id', 'email')
--- issuers/models.py
+++ issuers/models.py
@@ -1,14 +1,23 @@
 from django.db import models
 
 class Issuer(models.Model):
+
+    STATUS_CHOICES = (
+        ('new', 'Nuevo'),
+        ('waiting', 'Esperando primer doc'),
+        ('active', 'Activo'),
+        ('inactive', 'Inactivto'),
+    )
+    status = models.CharField(max_length=255, choices=STATUS_CHOICES)
     slug = models.CharField(max_length=200)
     name = models.CharField(max_length=200)
+    info = models.TextField(blank=True, null=True)
     color = models.CharField(max_length=6, default='006EFF')
     avatar = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f'({self.status}) {self.name}'
 
 
 class IssuerEmail(models.Model):
